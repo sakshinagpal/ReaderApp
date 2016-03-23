49f05cf84fe61f7f307f77b42538e169fd038847
sakshinagpal <sakshi.nagpal@comprotechnologies.com>
2016-03-23 15:12:52 +0530

    adding project

---
+++ test/runner.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Sakshi on 3/23/2016.
+ */
+var protractorQA = require('gulp-protractor-qa');
+
+protractorQA.init({
+    runOnce: true, // optional: when you don't want to watch files changes
+    testSrc: './specs/*spec.js',
+    viewSrc: ['../src/login.html']
+})
